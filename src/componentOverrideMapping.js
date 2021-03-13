13217d8a200b5a441a7f4b23b88aa3d26849c2ff
Cody Nguyễn <cody@simicart.com>
2021-03-13 16:28:59 +0700

    Update componentOverrideMapping.js

--- src/componentOverrideMapping.js
+++ src/componentOverrideMapping.js
@@ -4,7 +4,7 @@
  */
 module.exports = componentOverrideMapping = {
     [`@magento/venia-ui/lib/RootComponents/Category/categoryContent.js`]: '@simicart/related_product/src/override/Category/categoryContent.js',
-    [`@magento/venia-ui/lib/components/ProductFullDetail/productFullDetail.js`]: '@simicart/related_product/src/override/productDetails/productFullDetail.js',
+    [`@magento/venia-ui/lib/components/ProductFullDetail/productFullDetail.js`]: '@simicart/related_product/src/override/ProductDetails/productFullDetail.js',
     [`@magento/venia-ui/lib/components/CartPage/cartPage.js`]: '@simicart/related_product/src/override/Cart/CartPage.js',
     [`@magento/venia-ui/lib/components/CheckoutPage/checkoutPage.js`]: '@simicart/related_product/src/override/Checkout/checkoutPage.js'
 
